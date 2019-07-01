5f148abbf14ed3a282659e15287116d73c5e2e33
Souvik Kundu <kivtech88@gmail.com>
2019-07-01 13:23:56 +0530

    commit

---
+++ jQueryMobileForm/jquery-mobile.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("input[type=submit]").click(function (e) {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        if (name == '' || email == '') {
+            e.preventDefault();
+            alert("Please Fill Required Fields");
+        }
+    });
+});
